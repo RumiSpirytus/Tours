b152c0c30e5c0446851a00985cfc1af739fe92c6
electrostrike <21020021@vnu.edu.vn>
2024-12-05 21:23:08 +0700

    commit

--- frontend/src/app/admin/(pages)/users/service.js
+++
@@ -1,7 +0,0 @@
-import AXIOS_API from "@/utils/axiosAPI";
-
-export async function deleteUser(id) {
-    const { data } = await AXIOS_API.delete(`/admin/user/${id}`)
-
-    return data
-}
--- frontend/src/utils/axiosAPI.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const baseURL = "http://localhost:3000/api/"
-
-const AXIOS_API = axios.create({
-    baseURL
-})
-
-export default AXIOS_API
